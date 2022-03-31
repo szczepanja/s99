f6b904b21be33576dc5f2b6c194fecb4c9ae02fb
a_szczepanik <a.szczepanik@adform.com>
2022-03-31 19:57:15 +0200

    s3-8 and tests

--- src/main/scala/Scala99.scala
+++ src/main/scala/Scala99.scala
@@ -1,9 +1,26 @@
 object Scala99 extends App {
 
-  def findLast[T](ls: List[T]): T = ls.last
+  def findLast[T](ls: Seq[T]): T = ls.last
 
-  def findLastButOne[T](ls: List[T]): T = ls.init.last
+  def findLastButOne[T](ls: Seq[T]): T = ls.init.last
 
-  def kthElement(n: Int, ls: List[Any]): Any = if (n >= 0) ls(n)
+  def kthElement(n: Int, ls: Seq[Any]): Any = if (n >= 0) ls(n)
 
+  def listLength(value: Seq[Any]) = value.size
+
+  def lengthRecursive(ls: Seq[Any]): Int = ls match {
+    case Nil => 0
+    case _ :: tail => 1 + lengthRecursive(tail)
+  }
+
+  def reverse[T](ls: Seq[T]): Seq[T] = ls.foldLeft(List[T]()) { (result, head) => head :: result }
+
+  def isPalindrome(ls: Seq[Int]): Boolean = ls.reverse == ls
+
+  def flatten(ls: Seq[Any]): Seq[Any] = ls flatMap {
+    case el: Seq[_] => flatten(el)
+    case e => Seq(e)
+  }
+
+  def compress(value: Seq[Char]): Seq[Char] = value.distinct
 }
--- src/test/scala/Scala99Spec.scala
+++ src/test/scala/Scala99Spec.scala
@@ -7,14 +7,42 @@ class Scala99Spec extends AnyFlatSpec with should.Matchers {
   import Scala99._
 
   "findLast" should "find last element in list" in {
-    findLast(List("ania", 'l', 3, 5.00, 0.3)) shouldBe 0.3
+    findLast(Seq("ania", 'l', 3, 5.00, 0.3)) shouldBe 0.3
   }
 
   "findLastButOne" should "find last element in list" in {
-    findLastButOne(List("ania", 'l', 3, 5.00, 0.3)) shouldBe 5.00
+    findLastButOne(Seq("ania", 'l', 3, 5.00, 0.3)) shouldBe 5.00
   }
 
   "kthElement" should "find kth element in list" in {
-    kthElement(2, List("ania", 'l', 3, 5.00, 0.3)) shouldBe 3
+    kthElement(2, Seq("ania", 'l', 3, 5.00, 0.3)) shouldBe 3
+  }
+
+  "listLength" should "return number of elements in list" in {
+    listLength(Seq("ania", 'l', 3, 5.00, 0.3)) shouldBe 5
+  }
+
+  "lengthRecursive" should "return number of elements in list" in {
+    lengthRecursive(Seq("ania", 'l', 3, "xyz", true, false, 0.3)) shouldBe 7
+  }
+
+  "reverse" should "reverse list" in {
+    reverse(Seq(1, 1, 2, 3, 5, 8)) shouldBe (Seq(8, 5, 3, 2, 1, 1))
+  }
+
+  "isPalindrome" should "return true" in {
+    isPalindrome(Seq(1, 2, 3, 2, 1)) shouldBe true
+  }
+
+  "isPalindrome" should "return false" in {
+    isPalindrome(Seq(1, 2, 3, 2, 4)) shouldBe false
+  }
+
+  "flatten" should "return flatten Seqs" in {
+    flatten(Seq(Seq(1, 1), 2, Seq(3, Seq(5, 8)))) shouldBe Seq(1, 1, 2, 3, 5, 8)
+  }
+
+  "compress" should "distinct duplicates" in {
+    compress(Seq('a', 'a', 'a', 'b', 'b', 'c', 'c', 'c', 'd', 'e', 'e')) shouldBe Seq('a', 'b', 'c', 'd', 'e')
   }
 }
