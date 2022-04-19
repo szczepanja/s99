9936aca39b1cdc1edc8b1e0599fce63b4ba6e531
a_szczepanik <a.szczepanik@adform.com>
2022-04-19 10:44:58 +0200

    Add method for shifting all string not characters

--- src/main/scala/CaesarCipher.scala
+++ src/main/scala/CaesarCipher.scala
@@ -1,11 +1,12 @@
 object CaesarCipher {
 
   def caesarCipher(c: Char, i: Int): Char = c match {
-    case c if c.isLetter => {
+    case c if c.isLetter =>
       val offset = if (c.isUpper) 'A' else 'a'
       (((c - offset + i) % 26) + offset).toChar
-    }
     case _ => c
   }
+
+  def caesar(s: String, n: Int): String = s.map(caesarCipher(_, n))
 
 }
--- src/test/scala/CaesarCipherSpec.scala
+++ src/test/scala/CaesarCipherSpec.scala
@@ -5,10 +5,25 @@ import matchers._
 
 class CaesarCipherSpec extends AnyFlatSpec with should.Matchers {
 
+  behavior of "caesarCipher"
+
   "caesarCipher" should "given a lowercase letter and a number between 0 and 26, return that letter Caesar shifted by that number" in {
     caesarCipher('a', 0) shouldBe 'a'
     caesarCipher('a', 1) shouldBe 'b'
     caesarCipher('a', 5) shouldBe 'f'
+
+  }
+
+  behavior of "caesar"
+
+  "caesar" should "given a string of lowercase letters and a number, return a string with each letter Caesar shifted by the given amount." in {
+    caesar("a", 1) shouldBe "b"
+    caesar("abcz", 1) shouldBe "bcda"
+    caesar("irk", 13) shouldBe "vex"
+    caesar("fusion", 6) shouldBe "layout"
+    caesar("dailyprogrammer", 6) shouldBe "jgorevxumxgsskx"
+    caesar("jgorevxumxgsskx", 20) shouldBe "dailyprogrammer"
+
   }
 
 }
